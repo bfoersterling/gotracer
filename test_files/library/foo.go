cae8c731d526effaa36587de412d4ba6b66d77d5
bfoersterling <bjoern.foersterling@gmail.com>
2025-01-25 08:36:16 +0100

    added method with pointer receiver in test_files/library

--- test_files/library/foo.go
+++ test_files/library/foo.go
@@ -11,6 +11,11 @@ func (f foo) print() {
 	fmt.Printf("%+v", f)
 }
 
+func (f *foo) set_default() {
+	f.index = 0
+	f.color = "black"
+}
+
 func (f foo) combine_bar() {
 	fmt.Printf(bar() + fmt.Sprintf("%v", f))
 }
